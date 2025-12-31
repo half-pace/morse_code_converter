3565e67b1b9440426ca07f0673fbedf74971de87
half pace <halfpace64@gmail.com>
2026-01-01 01:57:28 +0530

    Encoding block completed

--- morse_conv.py
+++ morse_conv.py
@@ -39,4 +39,17 @@ def morse(message, choice):
         #encoding block
         #formats the message received from the user
         formatted_message = message.upper().strip()
+        converted_message = "" #to store the resultant morse translation
         
+        #main encoding loop
+        for each_word in formatted_message:
+            if each_word in eng_code:
+                converted_message += eng_code[each_word]
+                converted_message += " " #to add 1 space between each letter of a single word for every word
+            elif each_word == " ": 
+                converted_message += " / " #if there's space between words we will add / to separate words
+            else:
+                print("Invalid Character Used!")
+                break
+        
+        return converted_message.strip()
